ab19c07fd661e55720af3e6ad196af70e4a23559
Diego Jose Albertini <guinchi02@hotmail.com>
2024-02-25 00:40:33 -0600

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Diego Albertini on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
